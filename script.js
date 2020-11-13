86551a8601db4559a482d80c261866b152a25177
agnesferenczi <65777475+agnesferenczi@users.noreply.github.com>
2020-11-13 19:35:03 +0100

    menü reszponzívan is kész

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const menu = document.querySelector('.header .nav-bar .nav-list .menu');
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const men_item = document.querySelector('.header .nav-bar .nav-list ul li a');
 const header = document.querySelector('.header.container');
 
 menu.addEventListener('click', () => {
@@ -10,8 +11,17 @@ menu.addEventListener('click', () => {
 document.addEventListener('scroll', () => {
     var scroll_position = window.scrollY;
     if (scroll_position > 170) {
-        header.style.backgroundColor = '#000000';
+        header.style.backgroundColor = 'rgb(31, 30, 30)';
     } else {
         header.style.backgroundColor = 'transparent';
     }
 });
+
+men_item.forEach((item) => {
+    item.addEventListener('click', () => {
+        menu.classList.toggle('active');
+        mobile_menu.classList.toggle('active');
+    });
+});
+
+/* Megfixelni ezt a fenti men-item-es hibát! */
